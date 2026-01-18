044c3475af8f763a3879dd473500eec226ad6fb0
dturkuler <dogan.turkuler@gmail.com>
2026-01-18 18:56:40 +0000

    chore(release): Bump version to 1.8.0

---
+++ tests/test_version.py
@@ -0,0 +1,7 @@
+import os
+from humandesign.utils.version import get_version
+
+def test_get_version():
+    """Test getting version from pyproject.toml."""
+    version = get_version()
+    assert version == "1.8.0"
